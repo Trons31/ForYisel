d8712a2862d0284c1bdf94cabdf6e3408b5fcec5
unknown <you@example.com>
2022-10-06 21:20:41 -0500

    PreDeploy

--- pages/cita.tsx
+++ pages/cita.tsx
@@ -11,38 +11,6 @@ import { NextPage } from 'next';
 
 const citaPage:NextPage = () => {
 
-    const [Message1, setMessage1] = useState(false);
-
-    const [Message2, setMessage2] = useState(true);
-
-
-    const heandleClick2 = () =>{
-        setMessage1(true)
-        setMessage2(false)
-     var colors = ['#bb0000', '#ffffff'];
-
-     conffeti({
-         zIndex: 999,
-         particleCount: 100,
-         spread: 160,
-         angle: -100,
-         origin: {
-           x: 1,
-           y: 0,
-         },colors : colors
-       })
-
-       conffeti({
-         zIndex: 999,
-         particleCount: 100,
-         spread: 160,
-         angle: -100,
-         origin: {
-           x: 0,
-           y: 0,
-         },colors : colors
-       })
- }
 
   return (
      <Layout>
