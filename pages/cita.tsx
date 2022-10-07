65818e2bb9b1858bfd4d5ee84f879bf06400ee98
unknown <you@example.com>
2022-10-06 21:18:26 -0500

    PredDeplyo

--- pages/cita.tsx
+++ pages/cita.tsx
@@ -47,69 +47,7 @@ const citaPage:NextPage = () => {
   return (
      <Layout>
         
-        {
-            Message2
-            ?(
-              <>
-              <Box justifyContent='center' alignItems='center' display='flex' >
-            <Typography fontWeight={600} fontSize={30} > Bienvenida </Typography>
-        </Box>
-
-        <Box 
-            display='flex'
-            alignItems='center'
-            justifyContent='center' 
-            marginTop={10}
-            padding='0px 10px'
-            >
-            
-            <Card sx={{ maxWidth: 345 }}>
-          <CardHeader
-            avatar={
-              <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                DR
-              </Avatar>
-            }
-            title=""
-            subheader="Frase de hoy"
-          />
-            <CardContent>
-            <Typography variant="body2" color="text.secondary">
-              Mi corazon se va de su sitio, se dsplaza a la derecha, de la clavicula en direccion al hombro se desliza por el brazo hasta la mano que lo cede al boligrafo. Desde alli se lanza al vacio de la hoja, arrastra sus pies por la pagina y se convierte en este poema que aseguran dos cosas:
-            </Typography>
-
-            <Typography variant="body2" color="text.secondary">
-             - Que mirarte es el ovlido a la tristeza 
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-             - Que escucharte es el cominenzo a la felicidad. 
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-             Es listo mi corazon, tiene buen gusto -como su dueño-, el tambien gusta de ti.
-            </Typography>
-          </CardContent>
-          
-          <Button onClick={  heandleClick2}  fullWidth  variant='contained' startIcon={<FavoriteIcon />} endIcon={ <FavoriteIcon />} className='btn-init' >
-           Continuar
-          </Button>
-        </Card>
-        </Box>
-              </>
-            )
-            :(
-             <Typography></Typography>
-            )
-        }
-
-        {
-           Message1 
-           ?(
-            <CitaYise />
-           )
-           :(
-            <Typography></Typography>
-           )
-        }
+        
 
 
      </Layout>
