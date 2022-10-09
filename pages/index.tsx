89095416f7443f44321044da3ff570c2e7b705ff
unknown <you@example.com>
2022-10-09 12:46:16 -0500

    ActualizacionD

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,8 @@ import NextLink  from 'next/link';
 
 import { Layout } from '../components/Layouts';
 import { CardJuego } from '../components/cards';
+import ReactAudioPlayer from 'react-audio-player';
+import { truncate } from 'fs';
 
 
 
@@ -19,16 +21,36 @@ const Home: NextPage = () => {
 
  
 
-   const [Message, setMessage] = useState(false);
 
-   
-
- 
-
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(true);
 
   const handleClickOpen = () => {
-    setOpen(true);
+
+    setOpen(false);
+    var colors = ['#bb0000', '#ffffff'];
+
+        conffeti({
+            zIndex: 999,
+            particleCount: 1000,
+            spread: 160,
+            angle: -100,
+            origin: {
+              x: 1,
+              y: 0,
+            },colors : colors
+          })
+
+          conffeti({
+            zIndex: 999,
+            particleCount: 1000,
+            spread: 160,
+            angle: -100,
+            origin: {
+              x: 0,
+              y: 0,
+            },colors : colors
+          })
+
   };
 
   const handleClose = () => {
@@ -36,100 +58,108 @@ const Home: NextPage = () => {
   };
 
 
-
+  const theme = useTheme();
 
   return (
     
     <Layout>
       
-      <Box>
+    
 
-{
-  
-  Message
-  ?(
-       <CardJuego />
-    )
-    :(
-     
-      <>
+    {
+      open
+      ?(
+       <>
+       
+       <Box display='flex' alignItems='center' justifyContent='center' >
+              <Typography  fontWeight={600} fontSize={18} >
+                Bienvenida 
+              </Typography>
+              <Typography sx={{ color: '#ff9100' }} fontSize={25} >Yisel Aleman</Typography>
+          </Box>
 
-      <Typography fontWeight={600} fontSize={25} > Lista ! </Typography>
+        <Box justifyContent='center' display='flex' >
+              <Button onClick={handleClickOpen} variant='contained' sx={{ background: '#2196f3', color: 'white' }} >
+                Frase de hoy
+              </Button>
+        </Box>
 
-      <Box 
-            display='flex'
-            alignItems='center'
-            justifyContent='center' 
-            marginTop={10}
-            padding='0px 10px'
-            >
-            
-            <Card sx={{ maxWidth: 345 }}>
-          <CardHeader
-            avatar={
-              <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                DR
-              </Avatar>
-            }
-            title=""
-            subheader="Jueves 6, 2022"
-          />
-          <CardContent>
-            <Typography mb={2} variant="body2" color="text.secondary">
-              Hola yise, bueno acontinuacion veraz lo que tengo para ti, espero que te guste y que me prometas que lo consideraras, por favor dame un respuesta.
-            </Typography>
-            <Divider />
-            <Typography mt={2} variant="body2" color="text.secondary">
-              Bueno no me queda mas que decir que puedes empezar
-            </Typography>
-          </CardContent>
+       </>
+      )
+      :(
+        <>
+            <Box 
+          display='flex'
+          mt={5}
+          justifyContent='center' 
+        >
+                <Card sx={{ maxWidth: 345 }}>
+              <CardHeader
+                avatar={
+                  <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                    DR
+                  </Avatar>
+                }
+                title="Con cariño para ti"
+                subheader="Domingo 9 de octubre"
+              />
+                <CardContent>
+                <Typography variant="body2" color="text.secondary">
+                 <strong>Hermosa señorita</strong> de <strong>ojos encantadores</strong> me permitiria pedirle amablamente y con todo el respeto que lea este lindo mensaje.
+                </Typography>
+
+                <Typography mt={5} variant="body2" color="text.secondary">
+                - Nunca pierdas la alegria de vivir, de compartir, de amar y de ser feliz, levanta  la mirada y confia en que todo saldra bien, y por si lo olvidas aqui estare para recordarte que al final todo estara bien.
+                </Typography>
+                <Typography mt={2} variant="body2" color="text.secondary">
+                 - Sabes tu sonrisa solo demora unos segundos o tal vez minutos, pero su efecto a durado toda mi vida.
+                </Typography>
+              </CardContent>
+
+            </Card>
+        </Box>
+
+        <Box
+        display='flex'
+        justifyContent='center'
+        mt={5}
+        >
+            <Card >
+          <Box sx={{ display: 'flex', }}>
+            <CardContent sx={{ flex: '1 0 auto' }}>
+              <Typography component="div" variant="h5">
+                Mi heart
+              </Typography>
+              <Typography variant="subtitle1" color="text.secondary" component="div">
+                Tu admirador #1
+              </Typography>
+            </CardContent>
           
-          {/* <Button onClick={  heanledClick }  fullWidth  variant='contained' startIcon={<FavoriteIcon />} endIcon={ <FavoriteIcon />} className='btn-init' >
-            ver mensaje
-          </Button>  */}
+        
+          <CardMedia
+            component="img"
+            sx={{ width: 120 }}
+            image="/love.jpg"
+            alt="Live from space album cover"
+          />
+        </Box>
 
-      <Button  fullWidth  variant='contained' className='btn-init' onClick={handleClickOpen}>
-       Empezar
-      </Button>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">
-          {"Terminos y Condiciones"}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Ok acontinuacion podras ver lo que he preparado para ti, pero, antes debes aceptar mis terminos y condiciones, son simples, dame la oportunidad.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-
-        <NextLink href='/cita'  > 
-            <Link>
-            <Button   fullWidth  variant='contained'  className='btn-init' >
-            Acepto
-            </Button> 
-            </Link>
-        </NextLink>
-
-        </DialogActions>
-      </Dialog>
+          <Box sx={{ display: 'flex',ml:1,mt:1,mb:1  }}  padding='0px 1px' width='96%' >
+                <ReactAudioPlayer
+                  src="/just.mp3"
+                  controls
+                />
+            </Box>
 
         </Card>
-        </Box>
-      
-      </>
-      
-     
-      
-    )
-      
- }
+      </Box>
 
-</Box>
+        <Button sx={{ mt:2 }}>
+            Ver letra
+        </Button>
+        </>
+      )
+    }
 
     </Layout>
   
